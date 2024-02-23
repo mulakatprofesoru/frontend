fc059c6b3a4634cb6892bc7a6ce006a26c2438fd
mgencer03 <mrtgncr733@gmail.com>
2024-02-23 23:19:30 +0300

    Deneme button connect to Interview page

--- src/pages/DenemePage.js
+++ src/pages/DenemePage.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 function DenemePage(){
+  const navigate = useNavigate();
+    const InterviewPage = () => {
+        navigate('/interview');
+    }
   return (
       <div className="deneme_contanier">
         <div className="deneme">
-          <button className="button">Deneme 1</button>
-          <button className="button">Deneme 2</button> 
-          <button className="button">Deneme 3</button>
-          <button className="button">Deneme 4</button>
-          <button className="button">Deneme 5</button>
-          <button className="button">Deneme 6</button>
-          <button className="button">Deneme 7</button>
-          <button className="button">Deneme 8</button>
-          <button className="button">Deneme 9</button>
-          <button className="button">Deneme 10</button>
+          <button onClick={InterviewPage} className="button">Deneme 1</button>
+          <button onClick={InterviewPage} className="button">Deneme 2</button> 
+          <button onClick={InterviewPage} className="button">Deneme 3</button>
+          <button onClick={InterviewPage} className="button">Deneme 4</button>
+          <button onClick={InterviewPage} className="button">Deneme 5</button>
+          <button onClick={InterviewPage} className="button">Deneme 6</button>
+          <button onClick={InterviewPage} className="button">Deneme 7</button>
+          <button onClick={InterviewPage} className="button">Deneme 8</button>
+          <button onClick={InterviewPage} className="button">Deneme 9</button>
+          <button onClick={InterviewPage} className="button">Deneme 10</button>
         </div>
       </div>  )
 };
