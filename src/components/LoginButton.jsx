0c394a365e6d7bc2350cd7f06562a69a1ef87e62
mgencer03 <mrtgncr733@gmail.com>
2024-03-20 14:46:02 +0300

    Frontend: When user login and sign up send the request to backend

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,6 +2,7 @@ import {React , useEffect} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginIcon from '@mui/icons-material/Login';
 import ProfileBox from "./ProfileBox";
+import axios from "axios";
 
 
 const LoginButton = (props) => {
@@ -14,11 +15,19 @@ const LoginButton = (props) => {
     try {
       formData.append('email', user.email);
       formData.append('password', user.email);
-      const response = await fetch('http://localhost:5000/api/users/addUser', {
-          method: 'POST',
-          body: formData,
-      });
-
+      const response = await axios.post('http://localhost:5000/api/users/addUser', formData);
+      if (response.ok) {
+          console.log('Data sent successfully to Flask using POST');
+      } else {
+          console.error('Failed to send data to Flask using POST');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    try {
+      formData.append('email', user.email);
+      formData.append('password', user.email);
+      const response = await axios.post('http://localhost:5000/api/users/login', formData);
       if (response.ok) {
           console.log('Data sent successfully to Flask using POST');
       } else {
@@ -33,7 +42,7 @@ const LoginButton = (props) => {
 
   return (
     <header>
-    <h1>Mülakat Profesörü</h1>
+    <h1><a href="/">Mülakat Profesörü</a></h1>
     <div className="buttons">
         {isAuthenticated === false ?<LoginIcon  fontSize={"large"} onClick={loginWithRedirect} /> : ""}
         {isAuthenticated === true ? <ProfileBox 
